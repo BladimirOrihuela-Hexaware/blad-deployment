03b6d6bc718b72743ddb7bba1504db1928293297
Bladimir Orihuela <bladimiro1@hexaware.com>
2022-11-03 15:49:09 -0600

    fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I'm really close to complete it safely</p>
+        <p>Im really close to complete it safely</p>
         <a
           className="App-link"
           href="https://reactjs.org"
