a7ad4a0a83b0eada306bb7f3c311e1bc81bf868b
Bladimir Orihuela <bladimiro1@hexaware.com>
2022-11-03 17:06:40 -0600

    change text displayed in main page

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Im really close to complete it safely</p>
+        <p>This is a test page</p>
         <a
           className="App-link"
           href="https://reactjs.org"
