fc40dea8ae4f1b19b493a14c8d81957a77144085
Bladimir Orihuela <bladimiro1@hexaware.com>
2022-11-03 13:49:33 -0600

    updated text in main page

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bladimir Orihuela</p>
+        <p>Message updated for QA visibility only</p>
         <a
           className="App-link"
           href="https://reactjs.org"
