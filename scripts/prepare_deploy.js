ae754ca6ec775a44439bd99ea1086ccdd063711e
Bladimir Orihuela <bladimiro1@hexaware.com>
2022-11-03 14:48:38 -0600

    log token

--- scripts/prepare_deploy.js
+++ scripts/prepare_deploy.js
@@ -4,6 +4,8 @@ const { execSync } = require("child_process");
 const token = process.env.NETLIFY_TOKEN;
 const siteId = process.env.SITE_ID;
 
+console.log(token, siteId);
+
 const output = execSync(`bash scripts/deploy.sh ${token} ${siteId}`, {
   encoding: "utf-8",
 });
